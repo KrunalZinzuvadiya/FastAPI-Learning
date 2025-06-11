592c1c8bdd263804b49d4150aaeec801383ed27b
KrunalZinzuvadiya <krunalsoni187@gmail.com>
2025-06-11 10:15:43 +0530

    Added new file and updated some code for 10 june

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,16 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
-SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind=engine)
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
-
-
-
 
 
 
---
+++ TodoApp/routers/auth.py
@@ -0,0 +1,47 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from pytest import Session
+from starlette import status
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+
+router = APIRouter()
+
+bcrypt_context = CryptContext(schemes = ['bcrypt'], deprecated = 'auto')
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name : str
+    last_name : str
+    password : str
+    role : str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.email,
+        first_name = create_user_request.first_name,
+        last_name = create_user_request.last_name,
+        role = create_user_request.role,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+
+    return create_user_model
+
+
+    return {'user': 'authenticated'}
