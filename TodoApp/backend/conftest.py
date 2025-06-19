da22fbbd7039fd617504cdb1a72fce56025d5cdd
KrunalZinzuvadiya <krunalsoni187@gmail.com>
2025-06-19 16:55:11 +0530

    Fixed bug of test

--- TodoApp/backend/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Get the absolute path of the project root directory
-project_root = os.path.dirname(os.path.abspath(__file__))
-
-# Add the project root directory to the Python path
-sys.path.insert(0, project_root) 
